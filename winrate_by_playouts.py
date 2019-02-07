6c884f04b9c8807ae16273c251c581380c24ffb1
Seth Troisi <sethtroisi@google.com>
2019-02-07 01:04:33 -0800

    new winrate_by_playouts.py

--- winrate_by_playouts.py
+++ winrate_by_playouts.py
@@ -1,16 +1,21 @@
 from collections import defaultdict
 import math
+from scipy import stats
 
-DATA = "data/wins_fast_slow.csv"
+DATA = "data/elo_by_speed.csv"
 
 with open(DATA) as f:
-    data = [list(map(int, line.split(","))) for line in f.readlines()]
+    data = [[int(i) if len(i) < 4 else i for i in line.split(",")] for line in f.readlines()]
 
+print("Games:", sum(r[3] for r in data))
+print("\tRapid:", sum(r[3] for r in data if r[2] == "rapid"))
+print("\tFast:", sum(r[3] for r in data if r[2] == "fast"))
+print("\tSlow:", sum(r[3] for r in data if r[2] == "slow"))
+print()
 print("Rows:", len(data))
-
 print("Models:", len(set(r[0] for r in data)))
 
-# Row is [black, white, is_slow, games, black_wins]
+# Row is [black, white, speed, games, black_wins]
 games = defaultdict(int)
 wins = defaultdict(int)
 
@@ -22,43 +27,67 @@ for r in data:
     games[tuple(r[:3])] += r[3]
     wins[tuple(r[:3])] += r[4]
 
-print ("Pairings:", len(data), len(games))
+for speed in ["rapid", "fast", "slow"]:
+    print (speed, "pairings:", len(set(tuple(r[:2]) for r in data if r[2] == speed)))
 
 def elo(key):
+    if not (key in wins and key in games):
+        return None
+
     winrate = wins[key] / games[key]
     winrate = min(0.99, max(0.01, winrate))
     return -400 * math.log10(1 / winrate - 1)
 
+def plot_and_save(diffs, name, a, b):
+    import matplotlib
+    matplotlib.use("Agg")
+
+    import matplotlib.pyplot as plt
+
+    plt.clf()
+
+    _ = plt.hist(x=diffs, bins=50)
+
+    plt.yscale("log")
+
+    plt.xlabel("Elo diff {} vs {} playouts".format(a, b))
+    plt.ylabel("Frequency")
+    plt.title("Elo difference at various playouts")
+
+    png = "pictures/elo_diff_histogram_{}.png".format(name)
+    plt.savefig(png)
+    print("eog", png)
+    s = stats.describe(diffs)
+    print("\tn={}, min={:.1f}, max={:.1f}, mean={:.1f}, stddev={:.1f}".format(
+        s.nobs, s.minmax[0], s.minmax[1], s.mean, s.variance ** 0.5))
+
 
 diff = []
+diff2 = []
+diff3 = []
 for key in sorted(games.keys()):
     a, b = key[:2]
 
-    if key[2] == True:
+    if key[2] != "slow":
         continue
 
-    slow_key = key[:2] + (False,)
-    fast_key = key[:2] + (True,)
+    rapid_key = key[:2] + ("rapid",)
+    fast_key = key[:2] + ("fast",)
+    slow_key = key[:2] + ("slow",)
 
-    slow_elo = elo(slow_key)
+    rapid_elo = elo(rapid_key)
     fast_elo = elo(fast_key)
-    diff.append(slow_elo - fast_elo)
+    slow_elo = elo(slow_key)
+    if slow_elo and fast_elo:
+        diff.append(slow_elo - fast_elo)
+    if slow_elo and rapid_elo:
+        diff2.append(slow_elo - rapid_elo)
+    if fast_elo and rapid_elo:
+        diff3.append(fast_elo - rapid_elo)
 
-    print ("{},{}: {:.4g} {:.4g} {:.4g}".format(a, b, slow_elo, fast_elo, slow_elo - fast_elo))
+#    print ("{},{}: {:.4g} {:.4g} {:.4g}".format(a, b, slow_elo, fast_elo, slow_elo - fast_elo))
 
-import matplotlib
-matplotlib.use('Agg')
-
-import matplotlib.pyplot as plt
-
-_ = plt.hist(x=diff, bins=50)
-
-plt.yscale('log')
-
-plt.grid(axis='y')
-plt.xlabel('Elo diff 800 vs 200 playouts')
-plt.ylabel('Frequency')
-plt.title('Elo difference at various playouts')
-
-plt.savefig('elo_diff_histogram.png')
-
+print()
+plot_and_save(diff, "slow_fast", 800, 200)
+plot_and_save(diff2, "slow_rapid", 800, 50)
+plot_and_save(diff3, "fast_rapid", 200, 50)
