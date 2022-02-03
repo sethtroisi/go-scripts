7461a55a783848f7f98999dc6f17930a2355e677
Seth Troisi <sethtroisi@google.com>
2022-02-02 17:52:01 -0800

    Last commit

---
+++ svm_to_card.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Read SVM_data.json and convert to "cards"
+#
+# ######### Policy    Value   :  0.13
+# # BOARD # A =  2.2   0  -0.2:  0.01
+# #       # B =  1.1   0.2-0.4:  0.23
+# # A   C # C =  0.1   0.4-0.6:  0.02
+# #    B  #            0.6-0.8: -0.01
+# #########            0.8-1.0: -0.11
+#
+#
+
+# TODO policy sharpness:
+# TODO policy other
+
